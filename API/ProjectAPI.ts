713882b72ecebc93d171ac232335e8b9e9f17fab
OlafGrzesiak <olaf.grzesiak@microsoft.wsei.edu.pl>
2025-06-08 15:04:45 +0200

    Konfiguracja środowiska, CRUD, API

---
+++ API/ProjectAPI.ts
@@ -0,0 +1,32 @@
+import { Project } from '../Models/Project';
+
+const STORAGE_KEY = 'projects';
+
+export class ProjectAPI {
+  static getAll(): Project[] {
+    const data = localStorage.getItem(STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+  }
+
+  static getById(id: string): Project | undefined {
+    return this.getAll().find(project => project.id === id);
+  }
+
+  static create(project: Project): void {
+    const projects = this.getAll();
+    projects.push(project);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(projects));
+  }
+
+  static update(updatedProject: Project): void {
+    const projects = this.getAll().map(project =>
+      project.id === updatedProject.id ? updatedProject : project
+    );
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(projects));
+  }
+
+  static delete(id: string): void {
+    const projects = this.getAll().filter(project => project.id !== id);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(projects));
+  }
+}
